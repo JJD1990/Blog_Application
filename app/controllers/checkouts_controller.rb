31e1080b2f7b4e71ebcc5681de7caa6404480bd6
jjd1990 <jack.douglas@rocketmail.com>
2022-08-12 16:12:32 +0100

    subscriptions in stripe added, possibvle bug with members only area?

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,22 +1,26 @@
 class CheckoutsController < ApplicationController
-before_action :authenticate_user!
-
+    before_action :authenticate_user!
+  
     def show
-        current_user.set_payment_processor :stripe
-        current_user.payment_processor.customer
-
-        @checkout_session = current_user
-            .payment_processor
-            .checkout(
-                mode: 'payment',
-                line_items: 'price_1LVxOMGiBYGD4KlC2Ej85hWI',
-                success_url: checkout_success_url
-            )
+      current_user.set_payment_processor :stripe
+      current_user.payment_processor.customer
+  
+      @checkout_session = current_user
+                          .payment_processor
+                          .checkout(
+                            mode: params[:payment_mode],
+                            line_items: params[:line_items],
+                            success_url: checkout_success_url
+                          )
     end
-
+  
     def success
-        @session = Stripe::Checkout::Session.retrieve(params[:session_id])
-        @line_items = Stripe::Checkout::Session.list_line_items(params[:session_id])
+      @session = Stripe::Checkout::Session.retrieve(params[:session_id])
+      @line_items = Stripe::Checkout::Session.list_line_items(params[:session_id])
     end
-    
-end
+  
+    # Get list of payments
+    # 3.0.3 :039 > Stripe::PaymentIntent.list({customer: User.first.pay_customers.first.processor_id}).each do |int|
+    #   3.0.3 :040 >   puts int.amount
+    #   3.0.3 :041 > end
+  end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_12_124227) do
+ActiveRecord::Schema.define(version: 2022_08_12_135450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -211,6 +211,9 @@ ActiveRecord::Schema.define(version: 2022_08_12_124227) do
     t.bigint "address_id"
     t.string "city"
     t.string "country"
+    t.string "subscription_status"
+    t.datetime "subscription_end_date", precision: 6
+    t.datetime "subscription_start_date", precision: 6
     t.index ["address_id"], name: "index_users_on_address_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
